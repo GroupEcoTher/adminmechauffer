9516b1860be860ce1f93458a9149d955e72de1c3
Djaxis <votre.email@example.com>
2024-09-17 19:59:16 +0200

    userhomedevis

--- src/pages/users/UsersHome.tsx
+++ src/pages/users/UsersHome.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { getData, getTotalUsers } from "../../config/firebase";
-import { collection, getDocs, getFirestore, setDoc, deleteDoc, getDoc } from "firebase/firestore";
+import { collection, getDocs, getFirestore, setDoc, deleteDoc, getDoc, doc, updateDoc } from "firebase/firestore";
 import { app } from "../../config/firebase";
 import './FullLengthBox.scss';
 import './UsersHome.scss';
-import { doc, updateDoc } from "firebase/firestore";
+
 
 // Ajout de la fonction personnalisée pour formater les timestamps Firebase
 const formatFirebaseTimestamp = (timestamp: any) => {
   if (!timestamp) return null;
-  const date = timestamp.toDate();
+  const date = new Date(timestamp);
   return date.toLocaleString();
 };
 
@@ -62,9 +62,11 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
   const [history, setHistory] = useState<any[]>([]);
   const [users, setUserState] = useState<any[]>([]);
   const [statusNonVerifieeUsers, setStatusNonVerifieeUsers] = useState<any[]>([]);
+  const [devis, setDevis] = useState<any[]>([]); // Ajout de l'état pour "devis"
   const [selectedCollection, setSelectedCollection] = useState<any[]>([]);
   const [selectedTitle, setSelectedTitle] = useState<string>('');
   const [expandedUsers, setExpandedUsers] = useState<{ [key: string]: boolean }>({});
+  const [expandedDevis, setExpandedDevis] = useState<{ [key: string]: boolean }>({});
   const [loading, setLoading] = useState<boolean>(false);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
@@ -76,6 +78,10 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
   const [totalAdresses, setTotalAdresses] = useState<number>(0);
   const [totalDemandes, setTotalDemandes] = useState<number>(0);
   const [totalStatusNonVerifieeUsers, setTotalStatusNonVerifieeUsers] = useState<number>(0);
+  const [totalDevis, setTotalDevis] = useState<number>(0); // Ajout de l'état pour le total des devis
+
+
+
 
   // Fetch des données Firestore pour "users" et "totalUsers"
   useEffect(() => {
@@ -92,6 +98,37 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
     };
     fetchData();
   }, []);
+
+
+
+
+  useEffect(() => {
+    const fetchDevis = async () => {
+      try {
+        const usersCollection = collection(db, 'users');
+        const usersSnapshot = await getDocs(usersCollection);
+        const allDevis: { id: string; [key: string]: any }[] = [];
+  
+        for (const userDoc of usersSnapshot.docs) {
+          const devisCollection = collection(userDoc.ref, 'devis');
+          const devisSnapshot = await getDocs(devisCollection);
+          devisSnapshot.forEach((devisDoc) => {
+            allDevis.push({ id: devisDoc.id, ...devisDoc.data() });
+          });
+        }
+  
+        setDevis(allDevis);
+        setTotalDevis(allDevis.length);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des devis :", error);
+      }
+    };
+  
+    fetchDevis();
+  }, []);
+
+
+  
 
   // Fetch des collections Firestore
   useEffect(() => {
@@ -111,6 +148,8 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
           setTotalArchiveUsers(querySnapshot.size);
         } else if (collectionName === 'demandes') {
           setTotalDemandes(querySnapshot.size);
+        } else if (collectionName === 'devis') {
+          setTotalDevis(querySnapshot.size);
         }
       } catch (error) {
         console.error(`Erreur lors de la récupération de la collection ${collectionName}:`, error);
@@ -122,6 +161,7 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
     fetchCollection('archiveUsers', setArchiveUsers);
     fetchCollection('demandes', setDemandes);
     fetchCollection('history', setHistory);
+    fetchCollection('devis', setDevis); // Ajout de la collection "devis"
   }, []);
 
   // Fonction pour mettre à jour l'état de l'utilisateur dans Firestore
@@ -143,6 +183,8 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
         await deleteDoc(userRef);  // Supprime l'utilisateur de 'archiveUsers'
       }
 
+
+
       // Mettre à jour l'état local pour refléter les changements
       setUserState((prevUsers) => {
         const updatedUsers = prevUsers.map((user) =>
@@ -150,6 +192,9 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
         );
         return updatedUsers;
       });
+
+
+
 
       // Mettre à jour les collections et leurs totaux
       if (updatedData.archived !== undefined) {
@@ -189,43 +234,6 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
     }
   };
 
-  /*
-  // Fonction pour récupérer toutes les collections
-  const fetchCollections = async () => {
-    try {
-      const fetchCollection = async (collectionName: string, setState: React.Dispatch<React.SetStateAction<any[]>>) => {
-        try {
-          const querySnapshot = await getDocs(collection(db, collectionName));
-          const dataList = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
-          setState(dataList);
-          
-          // Mettre à jour les totaux en fonction de la collection
-          if (collectionName === 'adresse') {
-            setTotalAdresses(querySnapshot.size);
-          } else if (collectionName === 'archiveUsers') {
-            setTotalArchiveUsers(querySnapshot.size);
-          } else if (collectionName === 'demandes') {
-            setTotalDemandes(querySnapshot.size);
-          }
-        } catch (error) {
-          console.error(`Erreur lors de la récupération de la collection ${collectionName}:`, error);
-        }
-      };
-      
-      // Appeler la fonction pour chaque collection
-      await fetchCollection('adresse', setAdresse);
-      await fetchCollection('archiveUsers', setArchiveUsers);
-      await fetchCollection('demandes', setDemandes);
-      await fetchCollection('history', setHistory);
-    } catch (error) {
-      console.error("Erreur lors de la récupération des collections:", error);
-    }
-  };
-  */
-
   // Fonction pour afficher ou cacher les détails de l'utilisateur
   const toggleUserDetails = (index: number) => {
     setExpandedUsers((prev) => ({
@@ -234,32 +242,75 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
     }));
   };
 
-  // Fonction pour afficher les utilisateurs par adresse e-mail
-  const renderUsersByEmail = (users: any[]) => {
-    const order = [
-      'dateCreation',
-      'id',
-      'role',
-      'status',
-      'civilite',
-      'firstname',
-      'lastname',
-      'Validationdocument',
-      'phone',
-      'tokenParrain',
-      'demandes',
-      'archived',
-      'standby'
-    ];
+  // Fonction pour afficher ou cacher la section "devis"
+  const toggleDevisDetails = (index: number) => {
+    setExpandedDevis((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
 
+
+
+
+  // Fonction pour afficher les clés et valeurs dans l'ordre spécifié
+  const renderObject = (obj: any, parentKey: string = '', index: number) => {
+    const keys = Object.keys(obj).sort((a, b) => {
+      // Ajoutez ici votre logique de tri si nécessaire
+      return a.localeCompare(b);
+    });
+  
+    return keys.map((key) => {
+      const value = obj[key];
+      const fullKey = parentKey ? `${parentKey}.${key}` : key;
+  
+      if (value === undefined) {
+        return null; // Ne pas afficher les valeurs "undefined"
+      }
+  
+      if (key === 'devis') {
+        return (
+          <li key={fullKey}>
+            <strong>{key}:</strong>
+            <button onClick={() => toggleDevisDetails(index)}>
+              {expandedDevis[index] ? '-' : '+'}
+            </button>
+            {expandedDevis[index] && (
+              <ul>{renderObject(value, fullKey, index)}</ul>
+            )}
+          </li>
+        );
+      }
+  
+      if (typeof value === 'object' && value !== null) {
+        return (
+          <li key={fullKey}>
+            <strong>{key}:</strong>
+            <ul>{renderObject(value, fullKey, index)}</ul>
+          </li>
+        );
+      }
+  
+      return (
+        <li key={fullKey}>
+          <strong>{key}:</strong> {fullKey.includes('date') ? formatFirebaseTimestamp(value) : String(value)}
+        </li>
+      );
+    });
+  };
+
+  // Fonction pour afficher les utilisateurs par adresse e-mail
+  const renderUsersByEmail = (users: any[], collectionName: string) => {
     return users.map((user, index) => (
       <li key={index} className="user-item">
         <div className="user-header">
           <div className="user-header">
+            {/* Action pour afficher les détails de l'utilisateur */}
             <div className="action" onClick={() => toggleUserDetails(index)}>
               <img src="/view.svg" alt="View Details" />
             </div>
 
+            {/* Lien pour envoyer un e-mail à l'utilisateur */}
             <a
               href={`mailto:${user.email || ''}`}
               className="user-email"
@@ -267,11 +318,15 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
             >
               {user.email || 'Email non disponible'}
             </a>
+
+            {/* Message de succès si la mise à jour de l'utilisateur a réussi */}
             {updatedUserId === user.id && (
               <div className="success-message">
                 Mise à jour réussie !
               </div>
             )}
+
+            {/* Message d'erreur si la mise à jour de l'utilisateur a échoué */}
             {errorUserId === user.id && (
               <div className="error-message">
                 Erreur lors de la mise à jour.
@@ -280,42 +335,51 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
           </div>
         </div>
 
+        {/* Afficher les détails de l'utilisateur si l'utilisateur est étendu */}
         {expandedUsers[index] && (
           <ul className="user-details expanded">
-            {order.map((key) => (
-              key !== 'archived' && key !== 'standby' && user[key] !== undefined && (
-                <li key={key} className={
-                  key === 'status' && user[key] === 'Vérifiée' ? 'status-verifiee' :
-                  key === 'status' && user[key] === 'Non Vérifiée' ? 'status-non-verifiee' :
-                  key === 'Validationdocument' && user[key] === true ? 'validationdocument-true' :
-                  key === 'Validationdocument' && user[key] === false ? 'validationdocument-false' : ''
-                }>
-                  <strong>{key}:</strong> {key === 'dateCreation' ? formatFirebaseTimestamp(user[key]) : String(user[key])}
+            {/* Afficher les propriétés de l'utilisateur */}
+            {renderObject(user, '', index)}
+
+            {/* Afficher les options "Archived" et "Standby" si la collection n'est pas 'adresse', 'history' ou 'demandes' */}
+            {collectionName !== 'adresse' && collectionName !== 'history' && collectionName !== 'demandes' && (
+              <>
+                <li className="select-container">
+                  <label htmlFor={`archived-${index}`}>Archived:</label>
+                  <select
+                    id={`archived-${index}`}
+                    value={user.archived ? "true" : "false"}
+                    onChange={(e) => updateUserState(user.id, { archived: e.target.value === "true" })}
+                  >
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                  </select>
                 </li>
-              )
-            ))}
-            <li className="select-container">
-              <label htmlFor={`archived-${index}`}>Archived:</label>
-              <select
-                id={`archived-${index}`}
-                value={user.archived ? "true" : "false"}
-                onChange={(e) => updateUserState(user.id, { archived: e.target.value === "true" })}
-              >
-                <option value="true">True</option>
-                <option value="false">False</option>
-              </select>
-            </li>
-            <li className="select-container">
-              <label htmlFor={`standby-${index}`}>Standby:</label>
-              <select
-                id={`standby-${index}`}
-                value={user.standby ? "true" : "false"}
-                onChange={(e) => updateUserState(user.id, { standby: e.target.value === "true" })}
-              >
-                <option value="true">True</option>
-                <option value="false">False</option>
-              </select>
-            </li>
+                <li className="select-container">
+                  <label htmlFor={`standby-${index}`}>Standby:</label>
+                  <select
+                    id={`standby-${index}`}
+                    value={user.standby ? "true" : "false"}
+                    onChange={(e) => updateUserState(user.id, { standby: e.target.value === "true" })}
+                  >
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                  </select>
+                </li>
+              </>
+            )}
+
+            {/* Affichage des devis à la fin */}
+            {user.devis && (
+              <li>
+                <strong>Devis:</strong>
+                <ul>
+                  {user.devis.map((devisItem: any, devisIndex: number) => (
+                    <li key={devisIndex}>{devisItem.name}</li>
+                  ))}
+                </ul>
+              </li>
+            )}
           </ul>
         )}
       </li>
@@ -327,39 +391,39 @@ const UsersHome: React.FC<UsersHomeProps> = ({ title }) => {
       <div className="home">
         <h1 className="page-title">{title}</h1>
         <FullLengthBox totalUsers={totalUsers} />
-
+  
         <div className="fullLengthBox">
-          {/* Boutons pour afficher les différentes collections */}
-          <button className="hoverEffect" onClick={() => { setSelectedCollection(users); setSelectedTitle('Users'); }}>Users<span className="total-users">{totalUsers ? `(${totalUsers})` : ''}</span>
-          </button>
-          <button className="hoverEffect" onClick={() => { setSelectedCollection(statusNonVerifieeUsers); setSelectedTitle('Status Non Vérifiée'); }}>Status Non Vérifiée<span className="total-users">{totalStatusNonVerifieeUsers ? `(${totalStatusNonVerifieeUsers})` : ''}</span>
-          </button>
-          <button className="hoverEffect" onClick={() => { setSelectedCollection(demandes); setSelectedTitle('Demandes'); }}>Demandes<span className="total-users">{totalDemandes ? `(${totalDemandes})` : ''}</span>
-          </button>
-          <button className="hoverEffect" onClick={() => { setSelectedCollection(adresse); setSelectedTitle('Adresse'); }}>Adresse<span className="total-users">{totalAdresses ? `(${totalAdresses})` : ''}</span>
-          </button>
-          <button className="hoverEffect" onClick={() => { setSelectedCollection(archiveUsers); setSelectedTitle('Archive Users'); }}>Archive Users<span className="total-users">{totalArchiveUsers ? `(${totalArchiveUsers})` : ''}</span>
-          </button>
-           <button className="hoverEffect" onClick={() => { setSelectedCollection(history); setSelectedTitle('History'); }}>History</button>
+          <button className="hoverEffect" onClick={() => { setSelectedCollection(users); setSelectedTitle('Users'); }}>Users<span className="total-users">{totalUsers ? `(${totalUsers})` : ''}</span></button>
+          <button className="hoverEffect" onClick={() => { setSelectedCollection(statusNonVerifieeUsers); setSelectedTitle('Status Non Vérifiée'); }}>Status Non Vérifiée<span className="total-users">{totalStatusNonVerifieeUsers ? `(${totalStatusNonVerifieeUsers})` : ''}</span></button>
+          <button className="hoverEffect" onClick={() => { setSelectedCollection(devis); setSelectedTitle('Devis'); }}>Devis<span className="total-users">{totalDevis ? `(${totalDevis})` : ''}</span></button>
+          <button className="hoverEffect" onClick={() => { setSelectedCollection(demandes); setSelectedTitle('Demandes'); }}>Demandes<span className="total-users">{totalDemandes ? `(${totalDemandes})` : ''}</span></button>
+          <button className="hoverEffect" onClick={() => { setSelectedCollection(adresse); setSelectedTitle('Adresse'); }}>Adresse<span className="total-users">{totalAdresses ? `(${totalAdresses})` : ''}</span></button>
+          <button className="hoverEffect" onClick={() => { setSelectedCollection(archiveUsers); setSelectedTitle('Archive Users'); }}>Archive Users<span className="total-users">{totalArchiveUsers ? `(${totalArchiveUsers})` : ''}</span></button>
+          <button className="hoverEffect" onClick={() => { setSelectedCollection(history); setSelectedTitle('History'); }}>History</button>
         </div>
       </div>
 
-      {/* Affichage de la collection sélectionnée par adresse e-mail */}
-      <div>
+      <div className="user-sections">
+        <div className="user-section">
+          <h2 className={selectedTitle === 'Status Non Vérifiée' ? 'collectionTitleOrange' : 'collectionTitle'}>
+            Collection Sélectionnée : {selectedTitle} - {selectedCollection.length ? `${selectedCollection.length}` : ''}
+          </h2>
 
-
-        <h2 className={selectedTitle === 'Status Non Vérifiée' ? 'collectionTitleOrange' : 'collectionTitle'}>
-        Collection Sélectionnée : {selectedTitle}  - {selectedCollection.length ? `${selectedCollection.length}` : ''}
-      </h2>
-
-
-
-        {loading && <div className="loader">Chargement...</div>} {/* Loader */}
-        {successMessage && <div className="success-message">{successMessage}</div>} {/* Message de succès */}
-        {errorMessage && <div className="error-message">{errorMessage}</div>} {/* Message erreur */}
-        <ul>
-          {renderUsersByEmail(selectedCollection)} 
-        </ul>
+          {loading && <div className="loader">Chargement...</div>}
+          {successMessage && <div className="success-message">{successMessage}</div>}
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <ul>
+            {selectedTitle === 'Devis' ? (
+              selectedCollection.map((devisItem) => (
+                <li key={devisItem.id}>
+                  {devisItem.email} - {devisItem.id} - {devisItem.firstname} - {devisItem.lastname} - {devisItem.phone}
+                </li>
+              ))
+            ) : (
+              renderUsersByEmail(selectedCollection, selectedTitle.toLowerCase())
+            )}
+          </ul>
+        </div>
       </div>
     </>
   );
